5e78d8dbff021e4692f0b9b2726373daf22d446f
Vu Huynh <vhuynh617@gmail.com>
2016-09-26 22:52:56 -0400

    Adding tagging exercises

--- functions.php
+++ functions.php
@@ -2,9 +2,64 @@
 
 $stylesheet_directory = get_stylesheet_directory();
 
+/**
+ * Include
+ */
 require_once( $stylesheet_directory . '/inc/class-criteo-onetag.php' );
 
 function criteostorefrontchild_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'criteostorefrontchild_enqueue_styles' );
+
+/**
+ * Override storefront_page_header function. Remove title from home page template.
+ */
+function storefront_page_header() {
+	?>
+	<header class="entry-header">
+		<?php
+		if ( ! is_page() ) {
+			storefront_post_thumbnail( 'full' );
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		}
+		?>
+	</header><!-- .entry-header -->
+	<?php
+}
+
+/**
+ * Override storefront_cart_link. Only include cart in primary navigation for woocommerce related pages.
+ */
+/*
+function storefront_cart_link() {
+	if ( is_woocommerce() || is_cart() || is_checkout() ) {
+		?>
+			<a class="cart-contents" href="<?php echo esc_url( WC()->cart->get_cart_url() ); ?>" title="<?php esc_attr_e( 'View your shopping cart', 'storefront' ); ?>">
+				<span class="amount"><?php echo wp_kses_data( WC()->cart->get_cart_subtotal() ); ?></span> <span class="count"><?php echo wp_kses_data( sprintf( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count(), 'storefront' ), WC()->cart->get_cart_contents_count() ) );?></span>
+			</a>
+		<?php
+	}
+}
+*/
+
+/**
+ * Don't display credit link.
+ */
+function criteostorefrontchild_storefront_credit_link() {
+	return false;
+}
+add_filter( 'storefront_credit_link', 'criteostorefrontchild_storefront_credit_link', 99 );
+
+/**
+ * Disable reviews.
+ *
+ * @param array $tabs Tabs for product detail page.
+ */
+function criteostorefrontchild_woocommerce_product_tabs( $tabs ) {
+
+	unset( $tabs['reviews'] );
+
+	return $tabs;
+}
+add_filter( 'woocommerce_product_tabs', 'criteostorefrontchild_woocommerce_product_tabs', 99 );
--- inc/class-criteo-onetag.php
+++ inc/class-criteo-onetag.php
@@ -1,6 +1,32 @@
 <?php
 /**
  * Class for rendering Criteo OneTags in Woocommerce.
+ *
+ * Exercises are triggered by category. The category of the first item in the cart/order are used to determine
+ * overall category. The catalog quality is 100%.
+ *
+ * Home Page:
+ * OK
+ *
+ * Listing:
+ * Jerseys - OK
+ * Hats - No product ID's in array
+ * Novelties - Product ID's are passed as string instead of items in an array
+ *
+ * Product:
+ * Jerseys - OK
+ * Hats - No product ID's passed resulting in JS error
+ * Novelties - Different ID type, not matching ID's in feed
+ *
+ * Basket:
+ * Jerseys - Child ID's passed which are not in feed.
+ * Hats - Missing comma between item objects for item field.
+ * Novelties - OK
+ *
+ * Sales:
+ * Jerseys - Prices are line totals, not unit price
+ * Hats - OK
+ * Novelties - Hardcoded transaction ID
  */
 
 class Criteo_OneTag {
@@ -36,7 +62,7 @@ class Criteo_OneTag {
 	 * @action wp_footer
 	 */
 	function action_wp_footer() {
-		if ( is_shop() ) {
+		if ( is_shop() || is_front_page() ) {
 			$this->render_home_page_tag();
 		} else if ( is_product_category() ) {
 			$this->render_listing_tag();
@@ -97,7 +123,13 @@ class Criteo_OneTag {
 				window.criteo_q.push(
 					{ event: "setAccount", account: <?php echo self::PARTNERID; ?> },
 					{ event: "setSiteType", type: "d" },
-					{ event: "viewList", item: [<?php echo esc_js( $product_ids ); ?>] }
+					<?php if ( is_tax( 'product_cat', 'hats' ) ) { ?>
+						{ event: "viewList", item: [] }
+					<?php } else if ( is_tax( 'product_cat', 'novelties' ) ) { ?>
+						{ event: "viewList", item: "<?php echo esc_js( $product_ids ); ?>" }
+					<?php } else { ?>
+						{ event: "viewList", item: [<?php echo esc_js( $product_ids ); ?>] }
+					<?php } ?>
 				);
 			</script>
 		<?php
@@ -112,6 +144,10 @@ class Criteo_OneTag {
 	 * Render Product tag.
 	 */
 	function render_product_tag() {
+		global $post;
+
+		$post_id = empty( $post->ID ) ? 0 : $post->ID;
+
 		$product_id = $this->get_sku();
 
 		ob_start();
@@ -122,7 +158,13 @@ class Criteo_OneTag {
 				window.criteo_q.push(
 					{ event: "setAccount", account: <?php echo self::PARTNERID; ?> },
 					{ event: "setSiteType", type: "d" },
-					{ event: "viewItem", item: <?php echo esc_js( $product_id ); ?> }
+					<?php if ( has_term( 'hats', 'product_cat' ) ) { ?>
+						{ event: "viewItem", item:  }
+					<?php } else if ( has_term( 'novelties', 'product_cat' ) ) { ?>
+						{ event: "viewItem", item: <?php echo esc_js( $post_id ); ?> }
+					<?php } else { ?>
+						{ event: "viewItem", item: <?php echo esc_js( $product_id ); ?> }
+					<?php } ?>
 				);
 			</script>
 		<?php
@@ -139,34 +181,54 @@ class Criteo_OneTag {
 		global $woocommerce;
 		$items = $woocommerce->cart->get_cart();
 
-		$basket_products_array = array();
+		if ( ! empty( $items ) ) {
 
-		foreach($items as $item => $values) {
+			$basket_products_array = array();
 
-			$product = $values['data'];
+			$first_post = '';
 
-			if ( $sku = $product->get_sku() ) {
-				$basket_products_array[] = '{ id: "' . esc_js( $sku ) . '", price: ' . esc_js( $product->price ). ', quantity: ' . esc_js( $values['quantity'] ) . ' }';
+			foreach( $items as $item => $values ) {
+
+				$product = $values['data'];
+
+				if ( empty( $first_post ) ) {
+					$first_post = $product->post;
+				}
+
+				if ( $sku = $product->get_sku() ) {
+
+					if ( has_term( 'jerseys', 'product_cat', $first_post ) ) {
+
+						$random_number = mt_rand( 0, 90 );
+						$padded_random_number = str_pad( $random_number, 2, '0', STR_PAD_LEFT );
+
+						$sku = $sku . $padded_random_number;
+					}
+
+					$basket_products_array[] = '{ id: "' . esc_js( $sku ) . '", price: ' . esc_js( $product->price ). ', quantity: ' . esc_js( $values['quantity'] ) . ' }';
+				}
 			}
+
+			$seperator = has_term( 'hats', 'product_cat', $first_post ) ? '' : ', ';
+
+			$basket_products = implode( $seperator, $basket_products_array );
+
+			ob_start();
+			?>
+				<script type="text/javascript" src="//static.criteo.net/js/ld/ld.js" async="true"></script>
+				<script type="text/javascript">
+					window.criteo_q = window.criteo_q || [];
+					window.criteo_q.push(
+						{ event: "setAccount", account: <?php echo self::PARTNERID; ?> },
+						{ event: "setSiteType", type: "d" },
+						{ event: "viewBasket", item: [<?php echo $basket_products; ?>] }
+					);
+				</script>
+			<?php
+			$onetag = ob_get_clean();
+
+			echo $onetag;
 		}
-
-		$basket_products = implode( ', ', $basket_products_array );
-
-		ob_start();
-		?>
-			<script type="text/javascript" src="//static.criteo.net/js/ld/ld.js" async="true"></script>
-			<script type="text/javascript">
-				window.criteo_q = window.criteo_q || [];
-				window.criteo_q.push(
-					{ event: "setAccount", account: <?php echo self::PARTNERID; ?> },
-					{ event: "setSiteType", type: "d" },
-					{ event: "viewBasket", item: [<?php echo $basket_products; ?>] }
-				);
-			</script>
-		<?php
-		$onetag = ob_get_clean();
-
-		echo $onetag;
 	}
 
 	/**
@@ -185,34 +247,52 @@ class Criteo_OneTag {
 		$order = wc_get_order( $order_id );
 		$items = $order->get_items();
 
-		$transaction_products_array = array();
+		if ( ! empty( $items ) ) {
 
-		foreach ( $items as $item ) {
+			$transaction_products_array = array();
 
-			$product = $order->get_product_from_item( $item );
+			$first_post = '';
 
-			if ( $sku = $product->get_sku() ) {
-				$transaction_products_array[] = '{ id: "' . esc_js( $sku ) . '", price: ' . esc_js( $product->price ). ', quantity: ' . esc_js( $item['qty'] ) . ' }';
+			foreach ( $items as $item ) {
+
+				$product = $order->get_product_from_item( $item );
+
+				if ( empty( $first_post ) ) {
+					$first_post = $product->post;
+				}
+
+				if ( $sku = $product->get_sku() ) {
+
+					$price = $product->price;
+
+					if ( has_term( 'jerseys', 'product_cat', $first_post ) ) {
+						$price = $item['item_meta']['_line_total'][0];
+					}
+
+					$transaction_products_array[] = '{ id: "' . esc_js( $sku ) . '", price: ' . esc_js( $price ). ', quantity: ' . esc_js( $item['qty'] ) . ' }';
+				}
 			}
+
+			$transaction_products = implode( ', ', $transaction_products_array );
+
+			$transaction_id = has_term( 'novelties', 'product_cat', $first_post ) ? 'TRANSACTION_ID' : $order_id;
+
+			ob_start();
+			?>
+				<script type="text/javascript" src="//static.criteo.net/js/ld/ld.js" async="true"></script>
+				<script type="text/javascript">
+					window.criteo_q = window.criteo_q || [];
+					window.criteo_q.push(
+						{ event: "setAccount", account: <?php echo self::PARTNERID; ?> },
+						{ event: "setSiteType", type: "d" },
+						{ event: "trackTransaction" , id: <?php echo esc_js( $transaction_id ); ?>, item: [<?php echo $transaction_products ; ?>] }
+					);
+				</script>
+			<?php
+			$onetag = ob_get_clean();
+
+			echo $onetag;
 		}
-
-		$transaction_products = implode( ', ', $transaction_products_array );
-
-		ob_start();
-		?>
-			<script type="text/javascript" src="//static.criteo.net/js/ld/ld.js" async="true"></script>
-			<script type="text/javascript">
-				window.criteo_q = window.criteo_q || [];
-				window.criteo_q.push(
-					{ event: "setAccount", account: <?php echo self::PARTNERID; ?> },
-					{ event: "setSiteType", type: "d" },
-					{ event: "trackTransaction" , id: <?php echo esc_js( $order_id ); ?>, item: [<?php echo $transaction_products ; ?>] }
-				);
-			</script>
-		<?php
-		$onetag = ob_get_clean();
-
-		echo $onetag;
 	}
 
 	/**
